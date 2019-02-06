bda7ce8baabc88afc4f0da71859b2f330fb3d593
Muhammed Najeem <yy53393@aeroes.internal>
2019-02-06 09:22:37 +0530

    added relations query

--- settings.py
+++ settings.py
@@ -1,2 +1,3 @@
 CLARIZEN_LOGIN_URL = 'https://apie1.clarizen.com/v2.0/services/authentication/login'
 CLARIZEN_DATA_QUERY_URL = 'https://apie1.clarizen.com/v2.0/services/data/query'
+CLARIZEN_RELATIONS_QUERY_URL = 'https://apie1.clarizen.com/v2.0/services/data/relationQuery'
