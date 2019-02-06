65888cdc63a706afe6818d1bea5ce04711d10805
Muhammed Najeem <yy53393@aeroes.internal>
2019-02-06 09:22:59 +0530

    table layout

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 import clarizen
-from dash_comps import indicate, progress, progress_status, circle_graph
+from dash_comps import indicate, progress, circle_graph
 
 projs = clarizen.get_subprojects("/Project/1nn8h0sy46ic07izfvbk84sm281")
+kpis = clarizen.work_items_by_topic("GAI KPI 2019")
 
-ext_css = ['https://raw.githubusercontent.com/tabler/tabler/dev/dist/assets/css/dashboard.css']
-app = dash.Dash(__name__, external_stylesheets=ext_css)
+app = dash.Dash(__name__)
 
 app.layout = html.Div(className='page', children=[
     html.Div(className='container-fluid', children=[
@@ -18,25 +18,39 @@ app.layout = html.Div(className='page', children=[
         html.Div(className='row row-cards', children=[
             html.Div(className='col-lg-2', children=[
                 html.Div(className='card', children=[
-                    circle_graph(55, 'Overall Progress'),
+                    html.Div(className='card-header', children=[
+                        html.H3(className='card-title', children='Overall Progress')
+                    ]),
+                    circle_graph(55),
                 ]),
             ]),
             html.Div(className='col-lg-2', children=[
                 html.Div(className='card', children=[
-                    circle_graph(65, 'KPI Progress'),
+                    html.Div(className='card-header', children=[
+                        html.H3(className='card-title', children='KPI Progress')
+                    ]),
+                    circle_graph(65),
                 ]),
             ]),
-            html.Table( 
-                [html.Tr([
-                    html.Th(x) for x in ['Name', 'Status', 'Manager', 'PercentComplete']    
-                ])] +
-                [html.Tr([
-                    html.Td(x['Name']),
-                    html.Td(indicate(x['TrackStatus']['Name'])),
-                    html.Td(x['ProjectManager']['Name']),
-                    html.Td(progress(x['PercentCompleted'])),
-                ]) for x in projs['entities']]
-            ),
+        ]),
+        html.Div(className='row row-cards row-deck', children=[
+            html.Div(className='col-12', children=[
+                html.Div(className='card', children=[
+                    html.Div(className='table-responsive', children=[
+                        html.Table(className='table table-hover table-outline table-vcenter card-table', children=[
+                            html.Tr([
+                                html.Th(x) for x in ['Name', 'Status', 'Manager', 'PercentComplete']    
+                            ])] +
+                            [html.Tr([
+                                html.Td(x['Name']),
+                                html.Td(indicate(x['TrackStatus']['Name'])),
+                                html.Td(x['ProjectManager']['Name']),
+                                html.Td(progress(x['PercentCompleted'])),
+                            ]) for x in projs['entities']]
+                        ),
+                    ])
+                ])
+            ])
         ])
     ])
 ])
