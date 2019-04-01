2c1e37186c55aec7197ca808f127f36554013a89
Najeem Muhammed <najeem@gmail.com>
2019-04-01 20:31:20 +0530

    Merged components and dash_comps

--- components.py
+++ components.py
@@ -1,9 +1,56 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_daq as daq
 
-from dash_comps import circle_graph, indicate
 from helper_functions import status_colour
+import plotly.graph_objs as go
+
+def indicate(status='On Track'):
+    return daq.Indicator(
+        color=status_colour(status),
+        value=True
+    )
+
+def progress(p):
+    return daq.GraduatedBar(
+        value=p/10
+    )
+
+def circle_graph(p, status='On Track', size=280, fontsize=30):
+    return dcc.Graph(
+        figure=go.Figure(
+            data=[
+                go.Pie(
+                    values = [p, 100-p],
+                    hole=0.8,
+                    showlegend=False,
+                    hoverinfo='none',
+                    textinfo='none',
+                    sort=False,
+                    title={
+                        'text':str(int(p)) + '%',
+                        'font': {
+                            'size':fontsize
+                        }
+                    }
+                )
+            ],
+            layout = {
+                'margin': {
+                    't':0.05*size,
+                    'b':0.05*size,
+                    'l':0.05*size,
+                    'r':0.05*size,
+                    'pad':0.05*size,
+                    'autoexpand':True,
+                },
+                'piecolorway':[status_colour(status), '#cccccc',],
+                'width':size,
+                'height':size,
+            }
+        ),
+    )
 
 def number_card(n, title, progress=None, pcolor='green'):
     if progress:
